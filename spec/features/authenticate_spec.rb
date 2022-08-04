62a2c348299d5f2162be7aa7b220879b8c115832
Sergei O. Udalov <sergei.udalov@gmail.com>
2022-08-04 14:35:41 +0300

    auth feature

--- spec/features/authenticate_spec.rb
+++ spec/features/authenticate_spec.rb
@@ -5,11 +5,24 @@ RSpec.feature "Authenticates", type: :feature do
 
   let!(:user) { FactoryBot.create :user, email: "admin@example.com", password: "secret123" }
 
+  before { FactoryBot.create :page, title: :main }
+  before { Capybara.current_driver = :selenium_chrome_headless }
+  after { Capybara.use_default_driver }
+
   it "requires auth to edit page" do
     visit "/pages/main/edit"
+
+    expect(current_url).to match "users/sign_in"
 
     fill_in "Email", with: "admin@example.com"
     fill_in "Password", with: "secret123"
     click_on "Log in"
+
+    visit "/pages/main/edit"
+    find("trix-editor").click.set("some content")
+    click_on "Update"
+
+    visit "/pages/main"
+    expect(page).to have_content "some content"
   end
 end
