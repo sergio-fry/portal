d6cf8d26cb923ea90033154ca484cfc840aa5e84
Sergei O. Udalov <sergei.udalov@gmail.com>
2022-08-23 08:09:37 +0300

    track versions

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,6 @@
 class Page < ApplicationRecord
   before_save :update_backlinks
-  after_save :sync_to_ipfs
+  after_save :sync_linked_pages
 
   has_many :links
   has_many :back_links, foreign_key: :target_page_id, class_name: "Link", autosave: true
@@ -36,18 +36,23 @@ class Page < ApplicationRecord
   end
 
   def sync_to_ipfs
-    if ipfs_cid != ipfs_new_content.cid
-      update_column :ipfs_cid, ipfs_new_content.cid
-      PingJob.perform_later(ipfs_new_content.url) if ENV.fetch("IPFS_PING_ENABLED", "false") == "true"
-      linked_pages.each(&:sync_to_ipfs)
-    end
+    self.ipfs_cid = ipfs_new_content.cid
+    PingJob.perform_later(ipfs_new_content.url) if ENV.fetch("IPFS_PING_ENABLED", "false") == "true"
   end
 
   def content=(value)
     super
 
+    return unless content_changed?
+
     add_new_links
     links.destroy removed_links
+    sync_to_ipfs
+    track_history
+  end
+
+  def track_history
+    # page_versions.build ipfs_cid: ipfs_cid
   end
 
   def removed_links
@@ -82,5 +87,9 @@ class Page < ApplicationRecord
     back_links.each do |link|
       link.slug = slug
     end
+  end
+
+  def sync_linked_pages
+    linked_pages.each { |page| ExportPageToIpfsJob.perform_later(page) }
   end
 end
---
+++ db/migrate/20220823045128_create_page_versions.rb
@@ -0,0 +1,13 @@
+class CreatePageVersions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :page_versions do |t|
+      t.integer :page_id
+      t.string :ipfs_cid
+
+      t.timestamps
+    end
+
+    add_foreign_key :page_versions, :pages
+    add_index :page_versions, :page_id
+  end
+end
