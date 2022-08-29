6f713cd8d0adb3add5b09690310e38d63600bece
Sergei O. Udalov <sergei.udalov@gmail.com>
2022-08-29 10:24:42 +0300

    remove unused job

--- app/jobs/export_page_to_ipfs_job.rb
+++
@@ -1,8 +0,0 @@
-class ExportPageToIpfsJob < ApplicationJob
-  queue_as :default
-
-  def perform(page)
-    page.sync_to_ipfs
-    page.save!
-  end
-end
--- spec/jobs/export_page_to_ipfs_job_spec.rb
+++
@@ -1,4 +0,0 @@
-require "rails_helper"
-
-RSpec.describe ExportPageToIpfsJob, type: :job do
-end
