4d58c3c7cf928c022f510113dc9e25fe4e6ffe58
Sergei O. Udalov <sergei.udalov@gmail.com>
2022-10-25 08:18:24 +0300

    dependent destroy

--- app/boundaries/boundaries/database/page.rb
+++ app/boundaries/boundaries/database/page.rb
@@ -6,7 +6,7 @@ module Boundaries
       has_many :back_links, foreign_key: :target_page_id, class_name: "Link", autosave: true
       has_many :linked_pages, through: :back_links, source: :page, class_name: "Page"
       has_many :linking_to_pages, through: :links, source: :target_page, class_name: "Page"
-      has_many :links
+      has_many :links, dependent: :destroy
       has_many :versions, class_name: "Boundaries::Database::PageVersion", autosave: true
 
       validates :slug, format: {with: /\A[a-zа-я0-9\-_]+\z/}, uniqueness: true, presence: true
