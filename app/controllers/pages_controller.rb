cb6067d0efe2184c19ef921f085fa85d1e1da43b
Sergei O. Udalov <udalov.x@mail.ru>
2023-05-24 08:01:22 +0300

    patch_01 08:01

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  before_action :set_page, only: %i[edit update destroy history]
+  before_action :set_page, only: %i[edit update destroy]
   before_action :set_page_aggregate, only: %i[show]
   include PagesHelper
 
