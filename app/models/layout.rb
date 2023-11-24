94b472ec3be3a06e9c6be86ce12f9d90d0611b69
Sergei O. Udalov <udalov.x@mail.ru>
2023-11-24 23:37:41 +0300

    counter update

--- app/models/layout.rb
+++ app/models/layout.rb
@@ -39,8 +39,7 @@ class Layout
                    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
                    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
                    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-                   ym(95675137, "init", {
+                   ym(95675134, "init", {
                         clickmap:true,
                         trackLinks:true,
                         accurateTrackBounce:true
