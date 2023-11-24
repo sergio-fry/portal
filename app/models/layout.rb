1f54841357415f1c1d238c4cdb33401e53727337
Sergei O. Udalov <udalov.x@mail.ru>
2023-11-24 19:05:50 +0300

    yandex metrika

--- app/models/layout.rb
+++ app/models/layout.rb
@@ -30,6 +30,25 @@ class Layout
           script type: 'text/javascript', src: js_from_vendor('jquery-3.6.1.min.js')
           script type: 'text/javascript', src: js_from_vendor('opal/runtime.js')
           script type: 'text/javascript', src: embed_js_url
+          script type: 'text/javascript' do
+            raw(
+              <<~SCRIPT
+                <!-- Yandex.Metrika counter -->
+                   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                   m[i].l=1*new Date();
+                   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                   ym(95675137, "init", {
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true
+                   });
+                <!-- /Yandex.Metrika counter -->
+              SCRIPT
+            )
+          end
         end
       end.to_s
     )
